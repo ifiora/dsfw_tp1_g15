9afb3ea07edbb580034e23099fb69b3914b82f1b
Ignacio Fiora <ignaciofiora@gmail.com>
2024-11-17 21:41:51 -0300

    Carga info JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, NavLink, Routes } from "react-router-dom";
 
@@ -10,45 +11,32 @@ import Api from "./api/Api";
 import ContactForm from "./ContactForm";
 
 export default function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch("/data/cardsData.json");
+        const data = await response.json();
+        setCards(data);
+      } catch (error) {
+        console.error("Error al cargar la informacion de las tarjetas:", error);
+      }
+    };
+
+    fetchCards();
+  }, []);
+
+  if (cards.length === 0) {
+    return <div>Cargando...</div>;
+  }
+
   const links = [
     { path: "/", text: "Inicio" },
     { path: "/fiora", text: "Fiora" },
     { path: "/dojas", text: "Dojas" },
     { path: "/bari", text: "Bari" },
     { path: "/api", text: "API" },
-  ];
-
-  const cards = [
-    {
-      name: "Ignacio Fiora",
-      description:
-        "Mi nombre es Ignacio, tengo 38 años. Trabajo como programador freelance y en mi tiempo libro disfruto practicar Taekwondo y jugar al Fútbol.",
-      link: {
-        href: "https://www.linkedin.com/in/ignacio-fiora-6a02b229/",
-        text: "Linkedin",
-      },
-      img: "foto_fiora.jpg",
-    },
-    {
-      name: "Javier Dojas",
-      description:
-        "Me gustan los deportes y los videojuegos. Actualmente estoy concentrándome en el running, poniéndole mucha dedicación para estar a punto para las carreras de 10km de fin de año.",
-      link: {
-        href: "https://www.instagram.com/javidodge",
-        text: "Instagram",
-      },
-      img: "foto_dojas.jpg",
-    },
-    {
-      name: "Pablo Bari",
-      description:
-        "Allá por el 2003 terminé mis estudios en sistemas, pero por problemas de la vida, nunca pude trabajar en el sector. Desde el año 2020 me propuse actualizarme en las nuevas tecnologías de desarrollo.",
-      link: {
-        href: "https://www.linkedin.com/in/pablo-bari-20b9a1214/",
-        text: "Linkedin",
-      },
-      img: "foto_bari.jpg",
-    },
   ];
 
   return (
@@ -61,15 +49,16 @@ export default function App() {
           ))}
         </ul>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/fiora" element={<Fiora data={cards[0]} />} />
-        <Route path="/dojas" element={<Dojas data={cards[1]} />} />
-        <Route path="/bari" element={<Bari data={cards[2]} />} />
-        <Route path="/api" element={<Api />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-
+      <div className="route-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fiora" element={<Fiora data={cards[0]} />} />
+          <Route path="/dojas" element={<Dojas data={cards[1]} />} />
+          <Route path="/bari" element={<Bari data={cards[2]} />} />
+          <Route path="/api" element={<Api />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
       <footer>
         <ContactForm></ContactForm>
       </footer>
