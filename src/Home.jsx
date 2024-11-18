02d22ee6c9eab84ad1e024be115d00c93b9e60f7
Ignacio Fiora <ignaciofiora@gmail.com>
2024-11-17 23:07:04 -0300

    estilos caratula

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,12 +5,21 @@ export default class Home extends React.Component {
     return (
       <section id="home">
         <h1>Trabajo Practico Grupal DSWF - Grupo 15</h1>
-        <h2>Integrantes:</h2>
-        <ul>
-          <li>Pablo Bari</li>
-          <li>Javier Dojas</li>
-          <li>Ignacio Fiora</li>
-        </ul>
+
+        <div className="caratula">
+          <ul>
+            <li>Pablo Bari</li>
+            <li>Javier Dojas</li>
+            <li>Ignacio Fiora</li>
+          </ul>
+
+          <ul>
+            <li className="title">Materia:</li>
+            <li className="indent">Desarrollo de Sistemas Web (Frontend)</li>
+            <li className="title">Profesor:</li>
+            <li className="indent">Luciano Ariel Martinez</li>
+          </ul>
+        </div>
       </section>
     );
   }
